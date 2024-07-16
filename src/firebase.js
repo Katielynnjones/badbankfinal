45ac41feb750114bc46796cd7c101e6007b5405f
Katielynnjones <146352847+Katielynnjones@users.noreply.github.com>
2024-07-16 13:30:40 -0500

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -5,13 +5,7 @@ import { getAuth } from 'firebase/auth';
 
 
 const firebaseConfig = {
-    apiKey: "AIzaSyB1sI99H7G3iiiLFeKFauhof_GLlu3bRyU",
-    authDomain: "katie-jonesbankingapp.firebaseapp.com",
-    databaseURL: "https://katie-jonesbankingapp-default-rtdb.firebaseio.com",
-    projectId: "katie-jonesbankingapp",
-    storageBucket: "katie-jonesbankingapp.appspot.com",
-    messagingSenderId: "105684370793",
-    appId: "1:105684370793:web:6d7170464ae1468af1e9f8",
+    config info goes here
     
 };
 
